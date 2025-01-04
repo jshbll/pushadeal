57e6e0ee642d91d1554182aca0fbbc126c4072ed
Josh <hello@mrjoshball.com>
2025-01-04 10:57:41 -0500

    fixed error

--- src/EmailTemplateBuilder.tsx
+++ src/EmailTemplateBuilder.tsx
@@ -1,4 +1,4 @@
-                        import React, { useState, ReactNode, ChangeEvent } from 'react';
+    import React, { useState, ReactNode, ChangeEvent } from 'react';
     import '@types/react';
     import './EmailTemplateBuilder.css';
 
