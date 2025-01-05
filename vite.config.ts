2b855dd5aaf1d31a4a4b28498b3ae05cc157e5a0
Josh <hello@mrjoshball.com>
2025-01-05 13:18:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     port: 5174,
   },
-  base: process.env.NODE_ENV === 'production' ? '/emaildispo.github.io/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/emaildispo/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
