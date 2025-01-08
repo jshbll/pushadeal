ee2ddd06a6c83bcd9c4479479964f7183c6b85fe
Josh <hello@mrjoshball.com>
2025-01-08 14:04:11 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     port: 5174,
   },
-  base: process.env.NODE_ENV === 'production' ? '/pushadeal/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/pushadeal.github.io/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
